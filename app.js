3bc808c322fb9c1a926562c422cdaca25ca8f134
ArkejGit <robert.krawczyk92@gmail.com>
2018-01-18 19:56:26 +0100

    Add validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require( 'express' );
 const exphbs = require( 'express-handlebars' );
 const mongoose = require( 'mongoose' );
+const bodyParser = require( 'body-parser' );
 
 const app = express();
 
@@ -23,6 +24,10 @@ app.engine( 'handlebars', exphbs( {
 } ) );
 app.set( 'view engine', 'handlebars' );
 
+// Body parser Middleware
+app.use( bodyParser.urlencoded( { 'extended': false } ) );
+app.use( bodyParser.json() );
+
 // Index Route
 app.get( '/', ( req, res ) => {
 	const title = 'Welcome';
@@ -40,6 +45,28 @@ app.get( '/about', ( req, res ) => {
 // Add Idea Form
 app.get( '/ideas/add', ( req, res ) => {
 	res.render( 'ideas/add' );
+} );
+
+// Process Form
+app.post( '/ideas', ( req, res ) => {
+	let errors = [];
+
+	if ( !req.body.title ) {
+		errors.push( { 'text': 'Please add a title' } );
+	}
+	if ( !req.body.details ) {
+		errors.push( { 'text': 'Please add some details' } );
+	}
+
+	if ( errors.length > 0 ) {
+		res.render( 'ideas/add', {
+			'errors': errors,
+			'title': req.body.title,
+			'details': req.body.details
+		} );
+	} else {
+		res.send( 'passed' );
+	}
 } );
   
 const port = 5000;
